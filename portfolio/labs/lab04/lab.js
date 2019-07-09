57a724883c5839f3ed7a77e116f12ac3fa3839b3
liao-pei-yu <52557498+liao-pei-yu@users.noreply.github.com>
2019-07-09 12:03:45 +0800

    ip2019s

--- portfolio/labs/lab04/lab.js
+++ portfolio/labs/lab04/lab.js
@@ -5,8 +5,24 @@ $(function(){
         var r = Math.floor((Math.random() * 4));
         $('#d2').append('<img src="./' + card[r] + '">')
     }
+
+$('#b1').on('click', function(){
+    $('.issue').hide();
+    $('#d2').html('');
+
+    for (var i = 0; i < 16; i++){
+        var r = Math.floor((Marh.random() * 4));
+        $('#d2').append('<img src="./joker-card.svg">');
+
+
+
+    }
 });
 
+
+
+
+});
 
 
 
